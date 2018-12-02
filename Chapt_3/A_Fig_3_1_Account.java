4e30a30717315e16978fb44bcbf9c74f36ac1686
arifulislamrana <ariful.islam0683@gmail.com>
2018-12-02 06:23:33 -0800

    Create A_Fig_3_1_Account.java

---
+++ Chapt_3/A_Fig_3_1_Account.java
@@ -0,0 +1,22 @@
+/**Account class that contains a name instance variable 
+ and methods to set and get its value*/
+//25-11-18//02:44am
+/**
+ *
+ * @author arif
+ */
+package Chapt_3;
+
+
+public class A_Fig_3_1_Account {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+}
