874eeb62c28277459f6844d57eadc66ea5e85ab0
arifulislamrana <ariful.islam0683@gmail.com>
2018-12-02 06:29:24 -0800

    Create A_Fig_5_1_WhileCounter.java

---
+++ Chapt_5/A_Fig_5_1_WhileCounter.java
@@ -0,0 +1,21 @@
+/**Counter Controlled iteration with the while iteration statement*/
+//30-11-18//12:30pm
+/**
+ *
+ * @author arif
+ */
+package Chapt_5;
+
+public class A_Fig_5_1_WhileCounter {
+    public static void main(String[] args) {
+        
+        int counter = 1;
+        
+        while(counter <= 10){
+            System.out.printf("%d ",counter);
+            ++counter;
+        }
+        
+        System.out.println();
+    }
+}
